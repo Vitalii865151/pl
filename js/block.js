3daa5cf27e106d0c11eb0f137b350a5cba686f9a
Vitalii865151 <114147019+Vitalii865151@users.noreply.github.com>
2025-05-29 15:40:56 +0300

    Update block.js

--- js/block.js
+++ js/block.js
@@ -22,6 +22,7 @@ document.addEventListener("DOMContentLoaded", function () {
     header.innerHTML = `
         <div class="header-menu">
             <a href="/pl/index.html" class="button_start">Strona główna</a>
+            <a href="/pl/zasady.html" class="button_start">Zasady</a>
             <a href="/pl/alphabet.html" class="button_start">Alfabet z transkrypcją</a>
             <a href="/pl/alphabet2.html" class="button_start">Próba alfabetu</a>
             <a href="/pl/words.html" class="button_start">Słowa</a>
